0ff15ec828b87ddec7117b778dd5e2d18cd4873b
Paulo Fonseca <email@paulofonseca.com>
2025-10-28 19:59:47 +0100

    another commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import { Providers } from './providers';
 import configData from '@/data/config.json';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Footer } from '@/components/Footer';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: `${configData.tokenName} Gallery`,
@@ -21,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Providers>
           <div className="flex flex-col min-h-screen">
             <div className="flex-1">
